be79273a9cdd0874864b7e6f8767b96e4bd5d68d
130-jwang3 <johnwang0726@gmail.com>
2021-06-10 19:22:50 -0700

    2021/6/10

---
+++ src/Q450.java
@@ -0,0 +1,35 @@
+public class Q450 {
+    /*
+    1.travel the bst to find the node to delete
+    2.find the smallest or the last leaf node andexchange its position to the deleted ntoe
+    3.return
+     */
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){
+            return null;
+        }
+        if(key < root.val){
+            root.left = deleteNode(root.left, key);
+        }else if(key > root.val){
+            root.right = deleteNode(root.right, key);
+        }else{
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+
+            TreeNode minNode = findMin(root.right);
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, root.val);
+        }
+        return root;
+    }
+
+    private TreeNode findMin(TreeNode node){
+        while(node.left != null){
+            node = node.left;
+        }
+        return node;
+    }
+}
---
+++ src/Q701.java
@@ -0,0 +1,21 @@
+public class Q701 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (val < root.val) {
+            if (root.left == null) {
+                TreeNode node = new TreeNode(val);
+                root.left = node;
+            } else {
+                insertIntoBST(root.left, val);
+            }
+        } else if (val > root.val) {
+            if (root.right == null) {
+                TreeNode node = new TreeNode(val);
+                root.right = node;
+            } else {
+                insertIntoBST(root.right, val);
+            }
+        }
+        return root;
+    }
+}
