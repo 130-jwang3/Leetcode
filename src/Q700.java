16d80d21e88fe52ab7abcbaa70a769f79e17a8f8
130-jwang3 <johnwang0726@gmail.com>
2022-02-08 21:06:32 -0800

    2022/2/8

---
+++ src/Q700.java
@@ -0,0 +1,12 @@
+public class Q700 {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
